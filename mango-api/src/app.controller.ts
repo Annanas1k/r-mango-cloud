22e411dda0554d2df3683e702dc5e3eb4dfec45d
radisimo <radu.pavlovschi@gmail.com>
2026-07-20 12:11:03 +0300

    configure nest with prisma ORM & postgres DB

--- mango-api/src/app.controller.ts
+++ mango-api/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('health/db')
+  async checkDb() {
+    try {
+      await this.prisma.$queryRaw`SELECT 1`;
+      return { status: 'ok', database: 'connected' };
+    } catch (error) {
+      throw new NotFoundException('error:', error);
+    }
   }
 }
--- mango-api/src/main.ts
+++ mango-api/src/main.ts
@@ -1,8 +1,15 @@
+console.log('main.ts loaded, starting bootstrap...');
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(` -> access poit: http://localhost:${process.env.PORT}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Bootstrap failed:', err);
+  process.exit(1);
+});
