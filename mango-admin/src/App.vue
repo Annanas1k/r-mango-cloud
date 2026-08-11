cf20ca31bbebf4e1a087f713ac5dd5b603a4b260
radisimo <radu.pavlovschi@gmail.com>
2026-08-11 15:31:12 +0300

    configura prime vue for ui components

--- mango-admin/src/App.vue
+++ mango-admin/src/App.vue
@@ -1,6 +1,16 @@
+<!-- App.vue -->
 <script setup lang="ts">
+import { Button } from 'primevue';
+import { ref } from 'vue'
+const visible = ref(false)
 </script>
 
 <template>
-  <h1>admin dashboard</h1>
+    <div class="p-6 flex flex-col gap-4">
+        <Button label="Deschide dialog" icon="pi pi-external-link" @click="visible = true" />
+        <Dialog v-model:visible="visible" header="Test" modal>
+            <p>PrimeVue + Tailwind + TS funcționează.</p>
+        </Dialog>
+        <Button >sdauhrfdui</Button>
+    </div>
 </template>
--- mango-admin/src/main.ts
+++ mango-admin/src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
-import './style.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.dark',
+        },
+    },
+})
+
+app.mount('#app')
